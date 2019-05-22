348e18840393e3e74cda693784e2481c97e70e0f
gonzalo <gonzalojgreco@gmail.com>
2019-05-22 12:49:40 -0300

    Clase 2

---
+++ Clase 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Clase_2
+{
+    enum TipoEmpleado
+    {
+        Empleado,
+        LU,
+        Contratado
+    }
+    struct Persona
+    {
+        public string Nombre;
+        public string Apellido;
+        public long Sueldo;
+        public TipoEmpleado Tipo;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Gonzalo";
+            persona.Apellido = "Greco";
+            persona.Sueldo = 100;
+            persona.Tipo = TipoEmpleado.LU;
+            
+            ImprimirPersona(persona);
+
+            var otraPersona = new Persona
+            {
+                Nombre = "Diego",
+                Apellido = "Gonzalez",
+                Sueldo = 200,
+                Tipo = TipoEmpleado.Empleado
+            };
+
+            ImprimirPersona(otraPersona);
+
+            Console.WriteLine(Sumar(2, 2, 5, 8, 7));
+
+            Console.WriteLine(Sumatoria(5));
+        }
+
+        public static void ImprimirPersona(Persona p)
+        {
+            Console.WriteLine(p.Nombre);
+            Console.WriteLine(p.Apellido);
+            Console.WriteLine(p.Sueldo);
+            Console.WriteLine(p.Tipo);
+        }
+
+        public static int Sumar(params int[] numeros)
+        {
+            int suma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                suma += numeros[i];
+            }
+            return suma;
+        }
+
+        public static int Sumatoria(int numero)
+        {
+            return (numero == 0) ? 0 : numero + Sumatoria(numero - 1);
+        }
+    }
+}
---
+++ Homework clase 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework_clase_2
+{
+    class Program
+    {
+        /*Crear un programa que divida 2 numero sin usar el operador / */
+        static void Main(string[] args)
+        {
+            int resto;
+            int cociente;
+
+            cociente = Dividir(10, 3, out resto);
+            Console.WriteLine("10 / 3");
+            Console.WriteLine("Cociente: " + cociente + " Resto: " + resto);
+
+            cociente = DividirRecursiva(10, 3, out resto);
+            Console.WriteLine("Recursiva");
+            Console.WriteLine("Cociente: " + cociente + " Resto: " + resto);
+        }
+
+        public static int Dividir(int dividendo, int divisor, out int resto)
+        {
+            int cociente = 0;
+            while(divisor <= dividendo)
+            {
+                cociente++;
+                dividendo -= divisor;
+            }
+            resto = dividendo;
+            return cociente;
+        }
+
+        public static int DividirRecursiva(int dividendo, int divisor, out int resto)
+        {
+            if(divisor <= dividendo)
+            {
+                resto = dividendo;
+                return 1 + DividirRecursiva(dividendo - divisor, divisor, out resto);
+            }
+                resto = dividendo;
+                return 0;
+        }
+    }
+}
