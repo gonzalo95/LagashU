0ba1a2504e970ec63d26997672bb99c7cddf892a
gonzalo <gonzalojgreco@gmail.com>
2019-05-31 12:35:25 -0300

    Clase 7

---
+++ Clase 7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
